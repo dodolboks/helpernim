747a480f7b5eb1e3bf79118dc2bd87ab3a4de73a
Alikusnadi <kugutsu.hiruko@gmail.com>
2023-09-25 00:56:49 +0700

    change claim data

--- helpJWT.nim
+++ helpJWT.nim
@@ -1,21 +1,17 @@
 
-import jwt, times, json, tables
+import jwt, json
 
 const secret = "65073f81ac36f0aeb7305d72.65073f87224972385a0f36f4.1694973808.abcdefghijklmnopqrstu.1234567890"
 
-# change object proc signJWT*(newType: NewObjek): string =
-proc signJWT*(uuid, email, name: string): string =
-  var token = toJWT(%*{
+proc signJWT*(data: string): string =
+  # load data to Json
+  let newData = parseJson(data)
+  let token = toJWT(%*{
     "header": {
       "alg": "HS256",
       "typ": "JWT"
     },
-    "claims": {
-      "uuid": uuid,
-      "email": email,
-      "name": name,
-      "exp": (getTime() + 100.days).toUnix() # 100 day exp
-    }
+    "claims": newData
   })
   token.sign(secret)
   result = $token
@@ -27,6 +23,7 @@ proc verifyJWT*(token: string): bool =
   except InvalidToken:
     result = false
 
+#need update
 proc decodeJWT*(token: string): string =
   let jwt = token.toJWT()
   result = $jwt.claims["uuid"].node.str  
